601c99e45ecf58d320b201cf64a5faf3c470cfb1
cristopherpaulino-cmd <cristopher.paulino@escola.pr.gov.br>
2026-08-17 16:28:51 -0300

    Create listaEntradas = [].py

---
+++ listaEntradas = [].py
@@ -0,0 +1,31 @@
+listaPedido = []
+valor = 0
+
+print("opções de entrada:")
+opcao = 0
+while opcao != 10:
+    print("----------------------------------")
+    opcao = int(input("Digite 1 para selecionar bruschettas 20R$\n2 para selecionar enroladinho de massa15R$\n3 para selecionar pão com pate15R$"))
+    if opcao == 1:
+       print("Sua entrada foi selecionada com sucesso!")
+       listaPedido.append
+       valor = 20
+    elif opcao == 2:
+        print("Sua entrada foi selecionada com sucesso!")
+        listaPedido.append
+        valor = 15
+    elif opcao == 3:
+        print("Sua entrada foi selecionada com sucesso!")
+        listaPedido.append
+        valor = 15
+print("opções de prato principal: ")
+opcao = 0
+while opcao != 10:
+    print("------------------------------------")
+    opcao = int(input("Digite 1 para selecionar batatas fritas com frango frito 45R$\n2 para selecionar macarrão a bolonhesa 40R$\n3 para selecionar feijoada 40R$"))
+    if opcao == 1:
+        print("Seu prato principal foi selecionado!")
+    elif opcao == 2:
+        print("Seu prato principal foi selecionado!")
+    elif opcao == 3:
+        print("Seu prato principal foi selecionado!")
